0cc2631db89395e5a07ed8c811f4b5a34a7ba59d
sergey <buvaka.sergey89@gmail.com>
2019-11-24 23:42:20 +0300

    tmp commit

--- app/src/main/java/com/wispapp/themovie/ui/main/MainActivity.kt
+++ app/src/main/java/com/wispapp/themovie/ui/main/MainActivity.kt
@@ -2,9 +2,11 @@ package com.wispapp.themovie.ui.main
 
 import android.graphics.Color
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.jaeger.library.StatusBarUtil
 import com.wispapp.themovie.R
 import com.wispapp.themovie.ui.base.BaseActivity
+
 
 class MainActivity : BaseActivity() {
 
@@ -12,5 +14,21 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         StatusBarUtil.setColor(this, Color.TRANSPARENT)
+
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
+//
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.menu_activity_main, menu)
+//        return true
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        when(item){
+//            findViewById<View>(R.id.action_search) ->{}
+//        }
+//        return super.onOptionsItemSelected(item)
+//    }
 }
