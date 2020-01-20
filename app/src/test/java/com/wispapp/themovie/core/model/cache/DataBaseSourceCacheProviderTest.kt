a6aa2bb638fbeff1ba32834bac3148f1c2fc7de8
sergey <industradamus@gmail.com>
2020-01-20 11:05:40 +0300

    Add test for cache

--- app/src/test/java/com/wispapp/themovie/core/model/cache/DataBaseSourceCacheProviderTest.kt
+++ app/src/test/java/com/wispapp/themovie/core/model/cache/DataBaseSourceCacheProviderTest.kt
@@ -10,8 +10,7 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mockito.`when`
-import org.mockito.Mockito.mock
+import org.mockito.Mockito.*
 import org.mockito.MockitoAnnotations
 
 class DataBaseSourceCacheProviderTest {
@@ -29,7 +28,12 @@ class DataBaseSourceCacheProviderTest {
 
     @Test
     fun getAllCachedData() {
-        val data = generateData()
+
+        val data = listOf(
+            ConfigModel(1, generateImageConfigModel(), generateChangeKeys()),
+            ConfigModel(2, generateImageConfigModel(), generateChangeKeys()),
+            ConfigModel(3, generateImageConfigModel(), generateChangeKeys())
+        )
 
         `when`(policy.isValid()).thenReturn(true)
         `when`(dao.getAll()).thenReturn(data)
@@ -37,11 +41,9 @@ class DataBaseSourceCacheProviderTest {
         provider = DataBaseSourceCacheProvider(policy, type, dao)
 
         val result = runBlocking { provider.getAll() }
-
         assertEquals(result is CacheState.Actual, true)
 
         val dataFromCache = (result as CacheState.Actual).data
-
         assertThat(dataFromCache.size, `is`(data.size))
         dataFromCache.forEachIndexed { index, model ->
             assertEquals(data[index], model)
@@ -49,19 +51,81 @@ class DataBaseSourceCacheProviderTest {
     }
 
     @Test
-    fun getById() {
+    fun getCachedDataById() {
+
+        val data = ConfigModel(1, generateImageConfigModel(), generateChangeKeys())
+
+        `when`(policy.isValid()).thenReturn(true)
+        `when`(dao.getById(1)).thenReturn(data)
+
+        provider = DataBaseSourceCacheProvider(policy, type, dao)
+
+        val resultSuccess = runBlocking { provider.getById(1) }
+        val resultNull = runBlocking { provider.getById(0) }
+
+        assertEquals(resultSuccess is CacheState.Actual, true)
+        assertEquals(resultNull is CacheState.Empty, true)
+
+        val dataFromCache = (resultSuccess as CacheState.Actual).data
+        assertEquals(dataFromCache, data)
+    }
+
+    @Test
+    fun getNoCachedAllData() {
+
+        `when`(policy.isValid()).thenReturn(false)
+
+        provider = DataBaseSourceCacheProvider(policy, type, dao)
+
+        val result = runBlocking { provider.getAll() }
+        assertEquals(result is CacheState.Empty, true)
+    }
+
+    @Test
+    fun getByIdNoCached() {
+
+        `when`(policy.isValid()).thenReturn(false)
+
+        provider = DataBaseSourceCacheProvider(policy, type, dao)
+
+        val result = runBlocking { provider.getById(1) }
+        assertEquals(result is CacheState.Empty, true)
     }
 
     @Test
     fun putAll() {
+
+        `when`(policy.getTimeStamp()).thenReturn(0L)
+        `when`(type.name).thenReturn(SourceType.CONFIG.name)
+
+        provider = DataBaseSourceCacheProvider(policy, type, dao)
+
+        val data = listOf(
+            ConfigModel(1, generateImageConfigModel(), generateChangeKeys()),
+            ConfigModel(2, generateImageConfigModel(), generateChangeKeys()),
+            ConfigModel(3, generateImageConfigModel(), generateChangeKeys())
+        )
+
+        runBlocking { provider.putAll(data) }
+        verify(dao).insertAll(data)
     }
 
     @Test
     fun put() {
+
+        `when`(policy.getTimeStamp()).thenReturn(0L)
+        `when`(type.name).thenReturn(SourceType.CONFIG.name)
+
+        provider = DataBaseSourceCacheProvider(policy, type, dao)
+
+        val data = ConfigModel(1, generateImageConfigModel(), generateChangeKeys())
+
+        runBlocking { provider.put(data) }
+        verify(dao).insert(data)
     }
 
-    private fun generateData(): List<ConfigModel> {
-        val configModel = ImagesConfigModel(
+    private fun generateImageConfigModel() =
+        ImagesConfigModel(
             baseUrl = "http://image.tmdb.org/t/p/",
             secureBaseUrl = "https://image.tmdb.org/t/p/",
             posterSizes = listOf("w92", "w154", "w185", "w342", "w500", "w780", "original"),
@@ -71,7 +135,8 @@ class DataBaseSourceCacheProviderTest {
             profileSizes = listOf("w45", "w185", "h632", "original")
         )
 
-        val changeKeys = listOf(
+    private fun generateChangeKeys() =
+        listOf(
             "adult", "air_date", "also_known_as", "alternative_titles", "biography",
             "birthday", "budget", "cast", "certifications", "character_names", "created_by",
             "crew", "deathday", "episode", "episode_number", "episode_run_time", "freebase_id",
@@ -82,10 +147,5 @@ class DataBaseSourceCacheProviderTest {
             "season", "season_number", "season_regular", "spoken_languages", "status",
             "tagline", "title", "translations", "tvdb_id", "tvrage_id", "type", "video", "videos"
         )
-        return listOf(
-            ConfigModel(1, configModel, changeKeys),
-            ConfigModel(2, configModel, changeKeys),
-            ConfigModel(3, configModel, changeKeys)
-        )
-    }
+
 }
